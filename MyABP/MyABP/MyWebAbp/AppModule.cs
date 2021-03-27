d36328dfa70ccc8346a11b4831a89c21c2715e2e
licl <licl@LICHUNLI>
2021-03-27 11:35:49 +0800

    试试

---
+++ MyABP/MyABP/MyWebAbp/AppModule.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Modularity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+namespace MyWebAbp
+{
+    [DependsOn(typeof(AbpAspNetCoreMvcModule))]
+    public class AppModule:AbpModule
+    {
+        /// <summary>
+        /// 配置中间件管道  相当于startup类中的Configure方法
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+
+            app.UseConfiguredEndpoints();
+        }
+    }
+}
---
+++ MyABP/MyABP/MyWebAbp/Controller/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace MyWebAbp.Controller
+{
+    public class HomeController : AbpController
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World");
+        }
+    }
+}
