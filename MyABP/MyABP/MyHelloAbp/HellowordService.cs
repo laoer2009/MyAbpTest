981f977c1c1dd2d33f68dd6b41028e878f9a7955
licl <licl@LICHUNLI>
2021-03-27 09:50:50 +0800

    初始化

---
+++ MyABP/MyABP/MyHelloAbp/HellowordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.DependencyInjection;
+
+namespace MyHelloAbp
+{
+  public   class HellowordService:ITransientDependency//瞬时注入  
+    {
+        public void Run()
+        {
+            Console.WriteLine($"Hello World {Guid.NewGuid().ToString()}");
+        }
+    }
+}
---
+++ MyABP/MyABP/MyHelloAbp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Volo.Abp;
+using Microsoft.Extensions.DependencyInjection;
+namespace MyHelloAbp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appliction = AbpApplicationFactory.Create<HelloAbpModule>();
+
+            appliction.Initialize();
+
+            var service = appliction.ServiceProvider.GetRequiredService<HellowordService>();
+
+            service.Run();
+
+            service = appliction.ServiceProvider.GetRequiredService<HellowordService>();
+
+            service.Run();
+
+        }
+    }
+}
